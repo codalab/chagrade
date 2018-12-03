8eaee011346e60ec122d78cf9a2d888028b1eec8
killbot <killbot@Killbot.com>
2018-12-02 21:35:39 -0700

    Added a setUp() function to reduce lines of code

--- apps/profiles/tests/test_smoke_tests.py
+++ apps/profiles/tests/test_smoke_tests.py
@@ -13,6 +13,11 @@ User = get_user_model()
 
 class ProfilesSmokeTests(TestCase):
 
+    def setUp(self):
+        User.objects.create_user(username='user', password='pass')
+        self.instructor = Instructor.objects.create()
+        self.klass = Klass.objects.create(instructor=self.instructor, course_number="1")
+
     def test_profiles_login_returns_200(self):
         resp = self.client.get(reverse('profiles:login'))
         assert resp.status_code == 200
@@ -24,7 +29,6 @@ class ProfilesSmokeTests(TestCase):
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Login and attempt to view change password page
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
         resp = self.client.get(reverse('profiles:change_password'))
         assert resp.status_code == 200
@@ -36,7 +40,6 @@ class ProfilesSmokeTests(TestCase):
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to sign-in as instructor
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
         resp = self.client.get(reverse('profiles:instructor_signup'))
         assert resp.status_code == 200
@@ -48,7 +51,6 @@ class ProfilesSmokeTests(TestCase):
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to see a users profile
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
         resp = self.client.get(reverse('profiles:instructor_signup'))
         assert resp.status_code == 200
@@ -60,7 +62,6 @@ class ProfilesSmokeTests(TestCase):
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to see the overview for students
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
         resp = self.client.get(reverse('profiles:student_overview'))
         assert resp.status_code == 200
@@ -72,7 +73,6 @@ class ProfilesSmokeTests(TestCase):
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to see the overview for instructors
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
         resp = self.client.get(reverse('profiles:my_profile'))
         assert resp.status_code == 200
@@ -84,156 +84,112 @@ class ProfilesSmokeTests(TestCase):
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to create klass
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
         resp = self.client.get(reverse('klasses:create_klass'))
         assert resp.status_code == 200
 
     def test_edit_klass_returns_200(self):
-        #  creating the instructor and klass objects for the test
-        instructor = Instructor.objects.create()
-        klass = Klass.objects.create(instructor=instructor, course_number="1")
-
         # Trying not logged in to edit klass
-        resp = self.client.get(reverse('klasses:edit_klass', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:edit_klass', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 302
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to edit klass as instructor
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
-        resp = self.client.get(reverse('klasses:edit_klass', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:edit_klass', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 200
 
     def test_klass_details_returns_200(self):
-        #  creating the instructor and klass objects for the test
-        instructor = Instructor.objects.create()
-        klass = Klass.objects.create(instructor=instructor, course_number="1")
-
         # Trying not logged in to view klass overview
-        resp = self.client.get(reverse('klasses:klass_details', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:klass_details', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 302
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to view klass overview
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
-        resp = self.client.get(reverse('klasses:klass_details', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:klass_details', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 200
 
     def test_klass_enrollment_returns_200(self):
-        #  creating the instructor and klass objects for the test
-        instructor = Instructor.objects.create()
-        klass = Klass.objects.create(instructor=instructor, course_number="1")
-
         # Trying not logged in to view klass enrollment
-        resp = self.client.get(reverse('klasses:klass_enrollment', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:klass_enrollment', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 302
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to view klass enrollment
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
-        resp = self.client.get(reverse('klasses:klass_enrollment', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:klass_enrollment', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 200
 
     def test_klass_homework_view_returns_200(self):
-        #  creating the instructor and klass objects for the test
-        instructor = Instructor.objects.create()
-        klass = Klass.objects.create(instructor=instructor, course_number="1")
-
         # Trying not logged in to view klass homework
-        resp = self.client.get(reverse('klasses:klass_homework', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:klass_homework', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 302
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to view klass homework
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
-        resp = self.client.get(reverse('klasses:klass_homework', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:klass_homework', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 200
 
     def test_klass_grading_homework_view_returns_200(self):
-        #  creating the instructor and klass objects for the test
-        instructor = Instructor.objects.create()
-        klass = Klass.objects.create(instructor=instructor, course_number="1")
-
         # Trying not logged in to view how badly you failed that klass, man
-        resp = self.client.get(reverse('klasses:klass_grading', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:klass_grading', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 302
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to view how badly you failed that klass, bro
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
-        resp = self.client.get(reverse('klasses:klass_grading', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:klass_grading', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 200
 
     def test_klass_activate_view_returns_200(self):
-        #  creating the instructor and klass objects for the test
-        instructor = Instructor.objects.create()
-        klass = Klass.objects.create(instructor=instructor, course_number="1")
-
         # Trying not logged in to view klass homework
-        resp = self.client.get(reverse('klasses:klass_activate', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:klass_activate', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 302
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to view klass homework
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
-        resp = self.client.get(reverse('klasses:klass_activate', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('klasses:klass_activate', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 200
 
     def test_homework_define_homework_returns_200(self):
-        #  creating the instructor and klass objects for the test
-        instructor = Instructor.objects.create()
-        klass = Klass.objects.create(instructor=instructor, course_number="1")
-
         # Trying not logged in to edit klass
-        resp = self.client.get(reverse('homework:define_homework', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('homework:define_homework', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 302
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to edit klass as instructor
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
-        resp = self.client.get(reverse('homework:define_homework', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('homework:define_homework', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 200
 
     def test_homework_grade_homework_view_returns_200(self):
         #  creating the instructor, klass, a fake student, and submission objects for the test
-        instructor = Instructor.objects.create()
-        klass = Klass.objects.create(instructor=instructor, course_number="1")
         faker = ChaUser.objects.create()
-        fakestudent = StudentMembership.objects.create(user=faker, klass=klass, student_id=1)
-        submission = Submission.objects.create(klass=klass, creator=fakestudent)
+        fakestudent = StudentMembership.objects.create(user=faker, klass=self.klass, student_id=1)
+        submission = Submission.objects.create(klass=self.klass, creator=fakestudent)
 
         # Trying not logged in to edit klass
         resp = self.client.get(reverse('homework:grade_homework',
-                                       kwargs={'klass_pk': klass.pk, 'submission_pk': submission.pk}))
+                                       kwargs={'klass_pk': self.klass.pk, 'submission_pk': submission.pk}))
         assert resp.status_code == 302
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to edit klass as instructor
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
         resp = self.client.get(reverse('homework:grade_homework',
-                                       kwargs={'klass_pk': klass.pk, 'submission_pk': submission.pk}))
+                                       kwargs={'klass_pk': self.klass.pk, 'submission_pk': submission.pk}))
         assert resp.status_code == 200
 
     def test_homework_submit_homework_view_returns_200(self):
-        #  creating the instructor and klass objects for the test
-        instructor = Instructor.objects.create()
-        klass = Klass.objects.create(instructor=instructor, course_number="1")
-
         # Trying not logged in to edit klass
-        resp = self.client.get(reverse('homework:submit_homework', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('homework:submit_homework', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 302
         assert resp.url.startswith(reverse('profiles:login'))
 
         # Trying while logged in to edit klass as instructor
-        User.objects.create_user(username='user', password='pass')
         self.client.login(username='user', password='pass')
-        resp = self.client.get(reverse('homework:submit_homework', kwargs={'klass_pk': klass.pk}))
+        resp = self.client.get(reverse('homework:submit_homework', kwargs={'klass_pk': self.klass.pk}))
         assert resp.status_code == 200
