037e17db5ae2683f83f77b63d5e3df35bc6b1f1d
gibsonbailey <gibson.bailey@live.com>
2019-11-05 12:22:45 -0800

    Removed old commented code.

--- chagrade/wsgi.py
+++ chagrade/wsgi.py
@@ -12,16 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 from whitenoise import WhiteNoise
-#
-# from my_project import MyWSGIApp
-#
-# application = MyWSGIApp()
-# application = WhiteNoise(application, root='/path/to/static/files')
-# application.add_files('/path/to/more/static/files', prefix='more-files/')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chagrade.settings.base")
 
 application = get_wsgi_application()
 
 application = WhiteNoise(application)
-#application.add_files('/path/to/more/static/files', prefix='more-files/')
