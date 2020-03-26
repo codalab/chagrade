13c8fd5605b7ad6987284dcd91444e36c193686d
Bailey Lind-Trefts <gibson.bailey@live.com>
2020-03-26 12:28:30 -0700

    Added private bucket name to heroku settings.

--- chagrade/settings/heroku.py
+++ chagrade/settings/heroku.py
@@ -16,6 +16,7 @@ AWS_S3_ENDPOINT_URL = cloudcube_base_url
 AWS_ACCESS_KEY_ID = os.environ.get('CLOUDCUBE_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('CLOUDCUBE_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = cloudcube_bucket
+AWS_STORAGE_PRIVATE_BUCKET_NAME = cloudcube_bucket
 AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL', 'private')
 AWS_QUERYSTRING_AUTH = False
 
