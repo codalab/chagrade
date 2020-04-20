3e32b2463299c53c95948e662eebc43b11201a27
Brendon Lind <brendonlind@hotmail.com>
2020-04-20 16:09:27 -0700

    Changed simple view for item Grade to detail view

--- apps/homework/models.py
+++ apps/homework/models.py
@@ -196,7 +196,7 @@ class Grade(models.Model):
     submission = models.ForeignKey('Submission', related_name='grades', on_delete=models.CASCADE)
     evaluator = models.ForeignKey('profiles.Instructor', related_name='assigned_grades', on_delete=models.CASCADE)
 
-    overall_grade = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    overall_grade = models.DecimalField(max_digits=6, decimal_places=1, default=0)
 
     text_grade = models.CharField(max_length=20, null=True, blank=True, default="0/0")
 
@@ -206,7 +206,7 @@ class Grade(models.Model):
     published = models.BooleanField(default=False)
 
     needs_review = models.BooleanField(default=True)
-    jupyter_notebook_grade = models.FloatField(null=True)
+    jupyter_notebook_grade = models.DecimalField(max_digits=6, decimal_places=1, default=0)
 
     def __str__(self):
         return "{0}:{1}".format(self.submission.github_url, self.evaluator.user.username)
@@ -320,7 +320,7 @@ class Criteria(models.Model):
 class CriteriaAnswer(models.Model):
     grade = models.ForeignKey('Grade', default=None, related_name='criteria_answers', on_delete=models.CASCADE)
     criteria = models.ForeignKey('Criteria', related_name='answers', on_delete=models.CASCADE)
-    score = models.IntegerField(default=0)
+    score = models.DecimalField(max_digits=6, decimal_places=1, default=0)
 
 
 class TeamCustomChallengeURL(models.Model):
