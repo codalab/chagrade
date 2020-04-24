70f8340c55d0b87b35d36c0dfefb506435079b5d
Bailey Lind-Trefts <gibson.bailey@live.com>
2020-04-24 08:37:28 -0700

    Conditional definiton of PublicStorage storage class based on whether being used for tests or development/production.

--- chagrade/settings/test.py
+++ chagrade/settings/test.py
@@ -1,3 +1,6 @@
 from .base import *
 
+
+TEST = True
+
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
--- chagrade/storage.py
+++ chagrade/storage.py
@@ -1,5 +1,11 @@
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class PublicStorage(S3Boto3Storage):
     default_acl = "public-read"
+
+
+if settings.TEST:
+    PublicStorage = FileSystemStorage
