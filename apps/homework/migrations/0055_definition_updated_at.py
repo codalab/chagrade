3d4be31c843dc2498d53fe2fa857eefc2d81311d
Bailey Lind-Trefts <gibson.bailey@live.com>
2020-05-07 10:04:02 -0700

    Fix migration dependency issue.

--- apps/homework/migrations/0055_definition_updated_at.py
+++ apps/homework/migrations/0055_definition_updated_at.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('homework', '0056_auto_20200317_0034'),
+        ('homework', '0054_auto_20200427_0136'),
     ]
 
     operations = [
